79eae2806ee0d10eda124b524249738e2c6fb3dc
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2017-11-09 12:32:39 -0500

    Prompt written.

--- CPP-PRG-11-10-Search-Function-for-the-Speakers-Bureau-Program/main.cpp
+++ CPP-PRG-11-10-Search-Function-for-the-Speakers-Bureau-Program/main.cpp
@@ -5,11 +5,17 @@
 //  Created by Keith Smith on 11/9/17.
 //  Copyright © 2017 Keith Smith. All rights reserved.
 //
+//  Add a function to Programming Challenge 9 (Speakers' Bureau) that allows the user to
+//  search for a speaker on a particular topic. It should accept a key word as an argument
+//  then search the array for a structure with that key word in the Speaking Topic field.
+//  All structures that match should be displayed. If no structure matches, a message saying
+//  so should be displayed.
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+int main()
+{
     return 0;
 }
